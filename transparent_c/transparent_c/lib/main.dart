14d0a43a329315d10cf0443c32d1cfa7abb246ea
nojj92 <63126953+nojj92@users.noreply.github.com>
2021-02-21 11:28:50 -0500

    Add files via upload

---
+++ transparent_c/transparent_c/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Transparen-C'),
+          backgroundColor: Colors.green,
+        ),
+        body: Text('Scan Barcode'),
+      ),
+    ),
+  );
+}
